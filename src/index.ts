a2d9bd1aac4f18b8eaf4f8b595f9da8a9d697ab6
krak <alexedtrifonov@gmail.com>
2024-02-12 16:12:27 +0100

    feat: implement api/users/{userId}

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,23 @@ const server = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url || '', true);
   const { pathname } = reqUrl;
 
+  //handle GET to see all users
   if (pathname === '/api/users' && req.method === 'GET') {
     const users = data.getUsers();
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(users));
+  
+    //handle GET request to open the user by ID
+  } else if (pathname?.startsWith('/api/users/') && req.method === 'GET') {
+    const userId = pathname.split('/')[3];
+    const user = data.getUserById(userId);
+    if (user) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(user));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
